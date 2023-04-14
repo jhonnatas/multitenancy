6553ab2dfba4ac29670bce99d319b9c6765ae05b
= <=>
2023-04-14 12:35:54 -0300

    Create controller Companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+  get 'companies/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ test/controllers/companies_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class CompaniesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get companies_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get companies_new_url
+    assert_response :success
+  end
+end
