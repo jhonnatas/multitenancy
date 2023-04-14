df3db94976e02043e13cec4c2df9329a9222f577
= <=>
2023-04-14 12:38:01 -0300

    Create Companies content views index and new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'companies/index'
-  get 'companies/new'
+  get 'companies/new', to: 'companies#new'
+     post 'companies/create'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'companies#index'
 end
